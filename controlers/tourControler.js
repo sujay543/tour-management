68d94e72f44bc7154061b6f8accc6276b2b7cedb
Sujay Sen <163733398+sujay543@users.noreply.github.com>
2026-01-17 02:06:21 +0530

    adding filter

--- controlers/tourControler.js
+++ controlers/tourControler.js
@@ -25,42 +25,16 @@ exports.checkBody = (req,res,next) => {
 }
 
 
-
 exports.getTour = async (req,res) =>{ 
 
-    //BUILD  the query
+    // //BUILD  the query
     const queryObject = {...req.query};
-    const excludeFields = ['sort','limit','page','fields'];
-    excludeFields.forEach(el => delete queryObject[el]);
+    const excludedfield = ['sort','page','field','limit'];
+    excludedfield.forEach(el => delete queryObject[el]);
+    const query = Tour.find(queryObject);
 
-    let query = Tour.find(queryObject);
-
-    if(req.query.sort){
-        query = query.sort(req.query.sort);
-    }else{
-        query = query.sort('createdAt');
-    }
-
-    if(req.query.fields){
-        const fields = req.query.fields.split(',').join(' ');
-        query = query.select(fields);
-    }else{
-        query = query.select('-__v');
-    }
-
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-
-    const skip = (page - 1)* limit;
-
-    query = query.skip(skip).limit(limit);
-    if(req.query.page){
-        const countdoc = await Tour.countDocuments();
-        if(skip >= countdoc) throw new Error('Page exceeds');
-    }
-    
-    //executet  the query
-    let tours = await query;
+    //Execute the query
+    const tours = await query;
 
     //send response
     res.status(200).json({
@@ -148,4 +122,22 @@ exports.deleteTour = async (req,res)=>{
     }
 
 
+}
+
+exports.getTourStats = async (req,res) => {
+    try{
+    const status = Tour.aggregate([
+        {
+            $match : { ratingAverage: { $gte: 4.5 } }
+        }
+    ])
+
+
+    }catch(err)
+    {
+        res.status(404).json(
+        {
+            status: "failed",
+        })
+    }
 }

c72b74646b1fb9d137b4a2c416db443bdde3a2c7
Sujay Sen <163733398+sujay543@users.noreply.github.com>
2026-02-03 13:35:10 +0530

    implementing a global error handling middleware

--- index.js
+++ index.js
@@ -21,12 +21,29 @@ app.use('/api/v1/tours',tourRoute);
 app.use('/api/v1/users',userRoute);
 
 app.all(/.*/,(req,res,next) => {
-  res.status(404).json({
-    status: 'Failed',
-    message: `Can't find this ${req.originalUrl}`
-  })
+  // res.status(404).json({
+  //   status: 'Failed',
+  //   message: `Can't find this ${req.originalUrl}`
+  // })
+
+  const err  = new Error(`Can't find this ${req.originalUrl} in this server`);
+  err.statusCode = 404;
+  err.status = 'failed';
+  next(err);
 })
 
+
+app.use((err,req,res,next) => 
+{
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json(
+    {
+      status: err.status,
+      message: err.message
+    }
+  )
+})
 
 module.exports = app;
 
